795893ad59bf00ea86dcaaa9a1533fede46e6953
Marco Cannata <marcocannata76@gmail.com>
2022-09-05 11:38:08 -0300

    File Upload testing section added

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -5,24 +5,42 @@ import org.openqa.selenium.WebDriver;
 
 public class HomePage {
     private final WebDriver driver;
+    
     public HomePage(WebDriver driver) {
         this.driver = driver;
     }
-    public HoversPage clickHovers (){
+    
+    public HoversPage clickHovers() {
         clickLink("Hovers");
         return new HoversPage(driver);
     }
+    
+    public KeyPressesPage clickKeyPresses() {
+        clickLink("Key Presses");
+        return new KeyPressesPage(driver);
+    }
+    
     public void clickLink(String linkText) {
         driver.findElement(By.linkText(linkText)).click();
     }
-    public DropdownPage cliclDropdown (){
+    
+    public DropdownPage cliclDropdown() {
         clickLink("Dropdown");
-         return new DropdownPage(driver);
+        return new DropdownPage(driver);
     }
-
+    
     public LoginPage clickFormAuth() {
         clickLink("Form Authentication");
         return new LoginPage(driver);
     }
-
+    
+    public AlertsPage clickJavaScriptsAlerts() {
+        clickLink("JavaScript Alerts");
+        return new AlertsPage(driver);
+    }
+    
+    public FileUploadPage clickFileUpload() {
+        clickLink("File Upload");
+        return new FileUploadPage(driver);
+    }
 }
--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -4,12 +4,12 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import pages.HomePage;
 
 public class BaseTests {
     private WebDriver driver;
     protected HomePage homePage;
-
     @BeforeClass
     public void setUp() {
         System.setProperty("webdriver.gecko.driver", "src/main/resources/geckodriver");
@@ -17,7 +17,11 @@ public class BaseTests {
         driver.get("https://the-internet.herokuapp.com/");
         homePage = new HomePage(driver);
     }
-
+    
+    @BeforeMethod
+    public void goHome(){
+        driver.get("https://the-internet.herokuapp.com/");
+    }
     @AfterClass
     public void tearDown() {
         driver.quit();
